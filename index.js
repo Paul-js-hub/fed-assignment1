453d72cc3d075000282ee6d887c3137b10522cf7
Paul-js-hub <hesponpoisson@gmail.com>
2022-05-09 02:42:27 +0300

    Align selected items

--- index.js
+++ index.js
@@ -1,99 +1,86 @@
-function createCanvas() {
-  this.canvas = new fabric.Canvas("c");
-}
+let canvas = window.canvas || new fabric.Canvas("c");
 
-function alignLeft() {
-  let left = document.getElementById("alignLeft");
-  let currentValue = left.getAttribute("data-action-type");
-  let currentObj = canvas.getActiveObject() || canvas.getActiveGroup();
-  if (currentValue != "" && currentObj) {
-    alignItems(currentValue, currentObj);
-    currentObj.setCoords();
+let group, previousDirection;
+
+canvas.on("selection:created", function(e) {
+    group = e.target
+});
+
+canvas.on("selection:cleared", function(e) {
+   group = previousDirection = undefined;
+});
+
+function updateGroup() {
+    // group.forEachObject(function(obj) {
+    //     group.removeWithUpdate(obj).addWithUpdate(obj);
+    // });
+    group.setCoords();
     canvas.renderAll();
-  } else {
-    return false;
-  }
 }
 
-function alignRight() {
-  let right = document.getElementById("alignRight");
-  let currentValue = right.getAttribute("data-action-type");
-  let currentObj = canvas.getActiveObject() || canvas.getActiveGroup();
-  if (currentValue != "" && currentObj) {
-    alignItems(currentValue, currentObj);
-    currentObj.setCoords();
+function spaceObjectsHorizontally() {
+    if (group === undefined) return;
+    const spacing = document.querySelector('#spacing').value;
+    const objs = group.getObjects();
+    objs.reduce((prevObj, obj) => {
+        const prevObjRect = prevObj.getBoundingRect();
+        obj.set({ left: prevObjRect.left + prevObj.getScaledWidth() + Number.parseInt(spacing) + (obj.getBoundingRect().left - obj.left) });
+        obj.setCoords();
+        return obj;
+    })
     canvas.renderAll();
-  } else {
-    return false;
-  }
 }
 
-function alignTop() {
-  let top = document.getElementById("alignTop");
-  let currentValue = top.getAttribute("data-action-type");
-  let currentObj = canvas.getActiveObject() || canvas.getActiveGroup();
-  if (currentValue != "" && currentObj) {
-    alignItems(currentValue, currentObj);
-    currentObj.setCoords();
-    canvas.renderAll();
-  } else {
-    return false;
-  }
+function alignObjectsHorizontally(direction) {
+    const factor = direction === 'left' ? 1 : -1;
+    if (group === undefined || previousDirection === direction) return;
+    group.forEachObject((obj) => {
+        obj.set({
+            left: -(factor * (group.getScaledWidth() / 2)) + (factor * (obj.getBoundingRect().width / 2)),
+        });
+        obj.setCoords();
+    });
+    updateGroup();
+    previousDirection = direction;
 }
 
-function alignBottom() {
-    let bottom = document.getElementById("alignBottom");
-    let currentValue = bottom.getAttribute("data-action-type");
-    let currentObj = canvas.getActiveObject() || canvas.getActiveGroup();
-    if (currentValue != "" && currentObj) {
-      alignItems(currentValue, currentObj);
-      currentObj.setCoords();
-      canvas.renderAll();
-    } else {
-      return false;
-    }
+function alignObjectsVertically(direction) {
+    const factor = direction === 'top' ? 1 : -1;
+    if (group === undefined || previousDirection === direction) return;
+    group.forEachObject((obj) => {
+        obj.set({
+            top: -(factor * (group.getScaledHeight() / 2)) + (factor * (obj.getBoundingRect().height / 2)),
+        });
+        obj.setCoords();
+    });
+    updateGroup();
+    previousDirection = direction;
 }
 
-function alignMiddle() {
-    let middle = document.getElementById("alignMiddle");
-    console.log("MIDD", middle)
-    let currentValue = middle.getAttribute("data-action-type");
-    let currentObj = canvas.getActiveObject() || canvas.getActiveGroup();
-    if (currentValue != "" && currentObj) {
-      alignItems(currentValue, currentObj);
-      currentObj.setCoords();
-      canvas.renderAll();
-    } else {
-      return false;
-    }
+function alignObjectsBottom() {
+    alignObjectsVertically('bottom')
 }
 
-function alignItems(val, currentObj) {
-  switch (val) {
-    case "left":
-      currentObj.set({
-        left: 0,
-      });
-      break;
-    case "right":
-      currentObj.set({
-        left: canvas.width - currentObj.width * currentObj.scaleX,
-      });
-      break;
-    case "top":
-      currentObj.set({
-        top: 0,
-      });
-      break;
-    case "bottom":
-      currentObj.set({
-        top: canvas.height - (currentObj.height * currentObj.scaleY)
-      })
-      break;
-    case "middle":
-      currentObj.set({
-        left: (canvas.width / 2) - ((currentObj.width * currentObj.scaleX) / 2)
-        })
-      break;
-    }
+function alignObjectsLeft() {
+    alignObjectsHorizontally('left');
+}
+
+function alignObjectsMiddle() {
+    if (group === undefined || previousDirection === 'middle') return;
+    group.forEachObject((obj) => {
+        obj.set({
+            left: 0,
+            top: 0,
+        });
+    });
+    updateGroup();
+    previousDirection = 'middle';
+}
+
+function alignObjectsRight() {
+    alignObjectsHorizontally('right');
+}
+
+function alignObjectsTop() {
+    alignObjectsVertically('top');
 }
